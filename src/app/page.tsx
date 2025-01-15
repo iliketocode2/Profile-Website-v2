ed5984b9a0d1ead9ece6bce6bebf9963291db934
William Goldman <60369048+iliketocode2@users.noreply.github.com>
2025-01-15 18:23:02 -0500

    final fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
 
           <div className="w-full max-w-4xl mx-auto space-y-12">
             <p className="text-xl leading-relaxed dark:text-white">
-              Hi, I'm Will! I am a freshman at Tufts University studying mechanical engineering and computer science. 
+              Hi, I&apos;m Will! I am a freshman at Tufts University studying mechanical engineering and computer science. 
               I love STEM related topics such as website design, aerospace engineering, and robotics. 
               I have experience programming with Python 2.7 and 3, C++, Rust, Java, HTML, CSS, JavaScript, 
               Typescript, and Swift. In my free time I love to watch Chelsea FC and go skiing!
