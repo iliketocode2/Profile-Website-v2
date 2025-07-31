737f840a30d106cb36ab5b03f3a8bfc1f75a9bb9
William Goldman <60369048+iliketocode2@users.noreply.github.com>
2025-07-31 16:20:23 -0400

    update hobbies

--- src/app/lib/hobbies.ts
+++ src/app/lib/hobbies.ts
@@ -4,41 +4,44 @@ export const hobbies: Hobby[] = [
     {
       title: "Band (Trumpet!)",
       image: "/images/band.jpg",
-      description: "Over the years I have become increasingly involved in my school's band program, moving on from just being a curious trumpet student to now being involved in pep rallies, Memorial Day celebrations, graduation performances, and even jazz band."
+      description: "In high school, I played trumpet in the concert band, performing at pep rallies, Memorial Day events, graduations, and with the jazz band. I still make time to play on the side in college!"
+
     },
     {
         title: "Lego",
         image: "/images/the lego room.jpg",
-        description: "I have always loved to build with Lego, and over the years my siblings and I have accumulated a large collection of sets. My favorite set is the Lion Knights' Castle!"
+        description: "I have always loved to build with Lego, and over the years my siblings and I have accumulated a large collection of sets. My favorite set is the Lion Knights' Castle."
     },
     {
         title: "Website Development",
         image: "/images/web design.png",
-        description: "After my taking my first HTML Web Design class in school, I have since developed a passion for designing and building my own websites!"
+        description: "After creating my first HTML page in school, I began developing full websites using modern tools like React.js and Next.js."
     },
     {
         title: "Robotics",
         image: "/images/FTC robot 2024.jpg",
-        description: "I got started with robotics in my Junior year in high school. Through my experience with the team I have learned a lot about leadership, fundraising, and the engineering design process. If you are interested robotics, I encourage you to connect with a local FIRST robotics group."
+        description: "Since captaining my FIRST Tech Challenge team 5276 in high school, I’ve continued to pursue robotics through the Tufts CubeSat team and personal projects."
+
     },
     {
         title: "3D Printing",
         image: "/images/3d printing.png",
-        description: "3D printing has allowed me to dive into various different design softwares from OnShape to Shapr3D and Fusion 360. I've also acquired an interest in various filament types and exploring their unique properties."
+        description: "3D printing sparked my interest in CAD software like OnShape, Shapr3D, and Fusion 360. I've also enjoyed experimenting with different filament types and their unique properties."
     },
     {
         title: "Chess",
         image: "/images/chess.png",
-        description: "I began playing chess during the pandemic and have since fallen in love with it! I was the captain of the Dover-Sherborn chess team for 3 years and I now enjoy playing daily games."
+        description: "I started playing chess during the pandemic and quickly became hooked! I captained the Dover-Sherborn chess team for three years and still play daily games."
+
     },
     {
-        title: "Mountain Biking",
-        image: "/images/bike.jpg",
-        description: "Mountain Biking is my favorite summer activity. Some of my coolest interactions with nature have been during early morning mountain biking trips when bears, owls, and other creatures are still roaming the woods."
+        title: "Soccer",
+        image: "/images/ChelseaLogo.png",
+        description: "I’m a big Chelsea FC fan and watch their matches on the weekends. I also love playing soccer with friends whenever I get the chance."
     },
     {
         title: "Skiing",
         image: "/images/Stowe 2023.jpg",
-        description: "Skiing is my favorite winter activity, especially when I'm with family and friends."
+        description: "Stowe Mountain Resort in Vermont is my favorite place to ski, but my dream is to hit the slopes in the Alps one day!"
     },
   ];
